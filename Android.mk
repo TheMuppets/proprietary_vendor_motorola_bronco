322edba36aeba471dd2ca0e9dd394d7f4493fecd
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-31 01:49:50 +0200

    bronco: add built-in firmware
    Change-Id: I1c7aa2829f5ad35750f79ca44e13223e29f4a4bb

--- Android.mk
+++ Android.mk
@@ -3,3 +3,27 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),bronco)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,41118f1d58eaf0f9b4f895a8235ce66225af6038)
+$(call add-radio-file-sha1-checked,radio/aop.img,6d923407fd1f487fcc08b50d4436f0ee33e45801)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,6bf5a563fa53d8d90c098718a07787cd152f9732)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,5698094995fca7353b0fab02011227d134d802a3)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,1800cf6f882ab6852e5229e80cadf366fadd8513)
+$(call add-radio-file-sha1-checked,radio/dsp.img,2d3baa4cbe9f95c6a8e3bbc643c1a716076d3a4b)
+$(call add-radio-file-sha1-checked,radio/fsg.img,54626a4b4eddc2578288d66bce363a9c5c51e4c7)
+$(call add-radio-file-sha1-checked,radio/hyp.img,56ed9eb53a33770ec25db95e1b188a78f42ce498)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,1293913a4fa1b30d3bbef8aa9d719ec97165a3f0)
+$(call add-radio-file-sha1-checked,radio/logo.img,346b018736ed9d4cfc2effa0f38097a2360525d2)
+$(call add-radio-file-sha1-checked,radio/modem.img,6e3bfabdd30d6fddef71a2fad29303238864eefd)
+$(call add-radio-file-sha1-checked,radio/prov.img,bed6026c98eaa1380cb9c0a491d5fb14ad601ff9)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,eb6a12ddf6271588f6dedfca622f0b23141ee339)
+$(call add-radio-file-sha1-checked,radio/shrm.img,cc6993261f8f273e0bde5f52d5633e9e5559f4f3)
+$(call add-radio-file-sha1-checked,radio/storsec.img,c0e7e7c19f55992267a7e366455ba41157278ae1)
+$(call add-radio-file-sha1-checked,radio/tz.img,84acc5720517a69d535f1f076a7eee4d94691d91)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,f57469bb21ba9da1b4e7f76a2cc0a017061f8284)
+$(call add-radio-file-sha1-checked,radio/xbl.img,067c86ef48228bf77d4e7b65f4502fa9b96fd4f5)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,c8a4af34dbd0c37d2734ca44df54d1ddb00238b8)
+
+endif

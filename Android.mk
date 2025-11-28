4d88127d267e67d44bd4215ab3437b20bcb15553
Esteban <esteuwu@proton.me>
2025-11-28 00:54:16 -0300

    bronco: Add missing proprietary firmware
    Change-Id: Ia65e58e693e472b1f0d33331a19b9a368ae1ec49

--- Android.mk
+++ Android.mk
@@ -8,6 +8,7 @@ ifeq ($(TARGET_DEVICE),bronco)
 
 $(call add-radio-file-sha1-checked,radio/abl.img,8adf50a93bd3d2eccdaea8a64da1dcfdcaaad16a)
 $(call add-radio-file-sha1-checked,radio/aop.img,67090fc0eba2e6fc4cd051d902726cd67d8510a9)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,bdeaf9ba1f080e9adcef76ad5b5b332eacd12afd)
 $(call add-radio-file-sha1-checked,radio/bluetooth.img,c59cbccc030200826728f7cc3bd7d003c6fa23a5)
 $(call add-radio-file-sha1-checked,radio/cpucp.img,5921ac7f7fb8d38c56e8ae2ccdf8770ecdc317a4)
 $(call add-radio-file-sha1-checked,radio/devcfg.img,7c1350d5d9b6fc604cc8765a32aeba2e0e53ea9a)
@@ -20,10 +21,13 @@ $(call add-radio-file-sha1-checked,radio/modem.img,5233516b8de64d73196ce199aab70
 $(call add-radio-file-sha1-checked,radio/prov.img,949f3d95e3193f8b3d19ad084648638d6b036e39)
 $(call add-radio-file-sha1-checked,radio/qupfw.img,916a910d60e9964a3716f256f1193ae363f6c253)
 $(call add-radio-file-sha1-checked,radio/shrm.img,8d2f07f0f44768d38057ab8950c52fe9bebc4be2)
+$(call add-radio-file-sha1-checked,radio/spss.img,a54b7036a7337dd536207a3198110ee36ddef01d)
 $(call add-radio-file-sha1-checked,radio/storsec.img,f09f0e637a65a67248873839d35625eb4294703c)
 $(call add-radio-file-sha1-checked,radio/tz.img,134a6ce56c368512c4b9c54d0ed1ec90f4cbc00e)
+$(call add-radio-file-sha1-checked,radio/uefi.img,e7b999df497e9ccce89de6dba47146a9ec71cd29)
 $(call add-radio-file-sha1-checked,radio/uefisecapp.img,60a88ffda304210a22e0252359bc329556be814c)
 $(call add-radio-file-sha1-checked,radio/xbl.img,3bbef921aeb7ddfda7dc668915871dc8dec7a4ba)
 $(call add-radio-file-sha1-checked,radio/xbl_config.img,f8f85cf95fbd944576a798df9e24fb214a437311)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,23ad0849895b22fee757ac30440294f3520bfc3c)
 
 endif
